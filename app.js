6257331a3ef22b90d4d119b777118af7c9d5849f
ahmet-parlak <ahmet--parlak@outlook.com>
2023-10-14 13:58:42 +0300

    Set Up Express Web Server with MongoDB
    - Created an Express web server.
    - Configured necessary settings and created middlewares.
    - Developed page router and page controller.
    - Successfully established a connection to MongoDB for the project.

---
+++ app.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const session = require('express-session');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+require('dotenv').config();
+
+const pageRouter = require('./routes/pageRoute');
+
+//DB CONNECTION
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log(`✔️  MongoDB connection is successful`))
+  .catch((err) => console.log(`❌ MongoDB connection is failed\n${err}`));
+
+//APP
+const app = express();
+
+//Configration
+app.set('view engine', 'ejs');
+
+//GLOBAL VARIABLES
+global.userIN = null;
+
+//MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(fileUpload());
+app.use(methodOverride('_method', { methods: ['GET', 'POST'] }));
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
+//ROUTES
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
+app.use('/', pageRouter);
+
+//LISTEN
+const port = process.env.PORT ?? 5000;
+app.listen(port, () => console.log(`🚀 The server running at port ${port}`));
